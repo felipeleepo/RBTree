9991c79cabe8a4178c495c9cb4fe46993eb4fce5
Felipe Barbosa <felipebinfo@gmail.com>
2018-10-15 16:37:59 -0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    Arvore rb = new Arvore();
+    rb.Inserir(3);
+    rb.Imprimir();
+    rb.Inserir(0);
+    rb.Imprimir();
+    rb.Inserir(1);
+    rb.Imprimir();
+    rb.Inserir(2);
+    rb.Imprimir();
+    rb.Inserir(4);
+    rb.Imprimir();
+    rb.Inserir(5);
+    rb.Imprimir();
+  }
+}
