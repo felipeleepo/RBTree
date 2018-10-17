cbde35a2206c563825c5c1749581b478edc757a6
Felipe Barbosa <felipebinfo@gmail.com>
2018-10-17 16:15:12 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -16,16 +16,32 @@ class Main {
     // Remocao Caso 2 
     // rb.Remover(4);
     
-    rb.Inserir(10);
-    rb.Inserir(6);
-    rb.Inserir(8);
+    /*rb.Inserir(15);
+    rb.Inserir(11);
+    rb.Inserir(9);
     rb.Inserir(4);
-//    rb.Inserir(2);
+    rb.Inserir(8);
+    rb.Inserir(5);
+    rb.Inserir(13);
+    rb.Inserir(7);
+    rb.Inserir(1);
+    rb.Inserir(2);
+    rb.Inserir(3);*/
     
-    rb.Imprimir();   
-    
-    // Remoca Caso 3.1
-    //rb.Remover(2);
+    // Remocao Caso 3.1
+    // rb.Remover(11);
+
+    rb.Inserir(7);
+    rb.Inserir(2);
+    rb.Inserir(1);
+    rb.Inserir(10);
+    rb.Inserir(4);
+    rb.Inserir(11);
+    rb.Inserir(8);
+    rb.Inserir(9);
+
+    rb.Remover(7);
+
 
     /*rb.Inserir(7);
     rb.Inserir(2);
